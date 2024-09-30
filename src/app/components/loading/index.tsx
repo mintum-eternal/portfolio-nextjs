80447266d32e42e373a65e6ec673edb81f83b9e1
Minh Tâm <tam.pm@intelin.com>
2024-09-30 17:28:31 +0700

    add scroll style

--- src/app/components/loading/index.tsx
+++ src/app/components/loading/index.tsx
@@ -1,10 +1,14 @@
 interface ILoadingComponent {
-  size?: string;
+  className: string;
 }
-function LoadingComponent(props: ILoadingComponent) {
+function LoadingComponent({
+  className,
+}: ILoadingComponent) {
   return (
     <div className="w-full h-full flex justify-center items-center">
-      <div className="w-14 h-14 animate-circle bg-transparent rounded-full border-[6px] border-light !border-l-[6px] !border-l-primary box-border"/>
+      <div
+        className={`w-14 h-14 animate-circle bg-transparent rounded-full border-[6px] border-light !border-l-[6px] !border-l-primary box-border ${className}`}
+      />
     </div>
   );
 }
